2726267310f9ac0d7add34a410b456025165ce9a
Caine Atkinson <rc0033222@student.riversidecollege.ac.uk>
2018-09-18 09:54:08 +0100

    added the code to make the picture move smoothly in a square

--- followpath/Form1.cs
+++ followpath/Form1.cs
@@ -16,5 +16,29 @@ namespace followpath
         {
             InitializeComponent();
         }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 1, pictureBox1.Location.Y);
+                await Task.Delay(1);
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y +1);
+                await Task.Delay(1);
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 1, pictureBox1.Location.Y);
+                await Task.Delay(1);
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 1);
+                await Task.Delay(1);
+            }
+        }
     }
 }
